fa8b7917d7ada8e42f2c5d6f1092552c4ef39856
Derick Daniel <derickdaniel44@gmail.com>
2024-06-21 00:52:27 +0530

    Enabled discovery server using Eureka

--- src/main/java/com/microservice/authservice/AuthServiceApplication.java
+++ src/main/java/com/microservice/authservice/AuthServiceApplication.java
@@ -2,13 +2,25 @@ package com.microservice.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthServiceApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate
+				= new RestTemplate();
+		return restTemplate;
 	}
 
 }
