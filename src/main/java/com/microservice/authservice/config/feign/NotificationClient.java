6f4ac3316a0f2587665d9fc9f3a0d2cc45012c31
Derick Daniel <derickdaniel44@gmail.com>
2026-01-29 04:15:49 +0530

    Add Resilience4j circuit breaker to NotificationClient
    Integrated Resilience4j by adding the 'resilience4j-spring-boot2' dependency and applying a circuit breaker to the NotificationClient's notifyAction method with a fallback.

--- src/main/java/com/microservice/authservice/config/feign/NotificationClient.java
+++ src/main/java/com/microservice/authservice/config/feign/NotificationClient.java
@@ -1,15 +1,27 @@
 package com.microservice.authservice.config.feign;
 
-
-import com.microservice.authservice.dto.NotificationRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import com.microservice.authservice.dto.NotificationRequest;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @FeignClient("notification-service")
 public interface NotificationClient {
+	
+	Logger log = LoggerFactory.getLogger(NotificationClient.class);
 
-    @PostMapping("/notification/action")
-    void notifyAction(@RequestBody NotificationRequest request);
+	@CircuitBreaker(name = "notifyActionCB", fallbackMethod = "notifyActionFallback")
+	@PostMapping("/notification/action")
+	void notifyAction(@RequestBody NotificationRequest request);
+
+	default void notifyActionFallback(Exception ex) {
+		log.error("Notifcation Service - notify action failed.");
+		log.error(ex.getMessage());
+	}
 
 }
